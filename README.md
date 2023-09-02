70f3534788a9ce670c1eb79d781d32dcee09ba0b
Loubna Sellam <loubnasellam49@gmail.com>
2023-09-02 19:20:24 +0200

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# meteo-app
-# meteo-app
---
+++ meteo-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'; 
+
+import React from 'react'
+import MeteoApp from './components/MeteoApp/MeteoApp';
+
+function App() {
+  return (
+    <div className='App'>
+      < MeteoApp/>
+
+    </div>
+  );
+}
+
+export default App;
